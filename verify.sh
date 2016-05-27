2a4deb5a7c3cdc795d2c412e5a1398a5ea7ad407
Madzia <zc.magda@gmail.com>
2016-05-27 17:21:50 +0200

    done

--- verify.sh
+++ verify.sh
@@ -3,14 +3,21 @@
 msg=$1
 
 if [[ "${msg:0:1}" = [a-zA-Z] ]] ; 
+then
+	if grep '^[0-9a-zA-Z]*$' <<<$msg ; 
 	then
-		if grep '^[-0-9a-zA-Z]*$' <<<$msg ; 
-			then
-				echo "Message is valid";
-			else
-				echo "Message is invalid";
+		check=`echo ${msg} | aspell list -l pl`
+
+		if [[ "${check}" == "" ]] ;
+		then
+			echo "Message is valid";
+		else
+			echo "Message is invalid";
 		fi
 	else
 		echo "Message is invalid";
+	fi
+else
+	echo "Message is invalid";
 fi
 
